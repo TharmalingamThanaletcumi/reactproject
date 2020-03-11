ccc771c49ab29d52717e36f970d6a4f84c6cbd4c
unknown <tharmalingam.thanaletcumi@gmail.com>
2020-03-11 16:23:43 +0100

    TestLifeCycle

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,7 @@
 /*Import libruary needed*/
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom'
 import './Test.css';
 
 class Test extends Component { 
@@ -53,7 +54,9 @@ class Test extends Component {
                     <ul>
                     <ul>{this.state.data.map((item) => <Testlist data={item}/>) }</ul>
                     </ul> 
-                    <TestConstructor nametest={this.state.nametest} />                               
+                    <TestConstructor nametest={this.state.nametest} />
+                    <TestUpdate/>  
+                    <TestLifeCycle/>                                              
                 </div>
             )
                
@@ -126,7 +129,63 @@ class TestUpdate extends React.Component{
         this.findDomNodeHandler2 = this.findDomNodeHandler2.bind(this);
     }
     findDomNodeHandler1(){
-        
+        var myDiv = document.getElementById('myDivOne');
+        ReactDOM.findDOMNode(myDiv).style.color ='red';
+    }
+    findDomNodeHandler2(){
+        var myDiv = document.getElementById('myDivTwo');
+        ReactDOM.findDOMNode(myDiv).style.color='blue';
+    }
+    render(){
+        return(
+            <div className="buttonTest">
+                <button onClick={this.findDomNodeHandler1}>Find Dom Node 1</button>
+                <button onClick={this.findDomNodeHandler2}>Find Dom Node 2</button>
+                <h3 id="myDivOne">Node 1</h3>
+                <h3 id="myDivTwo">Node 2</h3>
+            </div>
+        )
+    }
+}
+/** Testing life cycle of component **/
+class TestLifeCycle extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {hello:"JavaTpoint"};
+        this.changeState = this.changeState.bind(this)
+    }
+    render(){
+        return(
+            <div>
+                <h1>React life cycle test</h1>
+                <h3>Hello {this.state.hello}</h3>
+                <button onClick={this.changeState}>Cliquer ici</button>
+            </div>
+        )
+    }
+    changeState(){
+        this.setState({hello:"All great tuto"});
+    }
+    componentWillMount(){
+        console.log('Component will mount')
+    }
+    componentDidMount(){
+        console.log('Component did mount')
+    }
+    UNSAFE_componentWillReceiveProps(){
+        console.log('Component will receive props')
+    }
+    shouldComponentUpdate(newProps, newState){
+        return true;
+    }
+    UNSAFE_componentWillUpdate(nextProps, nextState){
+        console.log('Component will Update')
+    }
+    componentDidUpdate(prevProps, prevState){
+        console.log('Component did update')
+    }
+    componentWillUnmount(){
+        console.log('Component will Unmount')
     }
 }
 
