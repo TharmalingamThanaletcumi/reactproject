e0d6c97509afacfc6a99de0a92a057b759de8585
unknown <tharmalingam.thanaletcumi@gmail.com>
2020-03-13 10:28:26 +0100

    Continue_lifeCycle

--- src/Test.js
+++ src/Test.js
@@ -11,34 +11,42 @@ class Test extends Component {
             data:
             [
                 {
+                    "id":"1",
                     "name":"toto",
                     "age":"25"
                 },
                 {
+                    "id":"2",
                     "name":"titi",
                     "age":"26"
                 },
                 {
+                    "id":"3",
                     "name":"yiyi",
                     "age":"18"
                 },
                 {
+                    "id":"4",
                     "name":"momo",
                     "age":"15"
                 },
                 {
+                    "id":"5",
                     "name":"tutu",
                     "age":"27"
                 },
                 {
+                    "id":"6",
                     "name":"yoyo",
                     "age":"18"
                 },
                 {
+                    "id":"7",
                     "name":"mimi",
                     "age":"17"
                 },
                 {
+                    "id":"8",
                     "name":"tata",
                     "age":"28"
                 }
@@ -52,7 +60,7 @@ class Test extends Component {
                 <div className="detailperso">
                     <Testname/>
                     <ul>
-                    <ul>{this.state.data.map((item) => <Testlist data={item}/>) }</ul>
+                    <ul>{this.state.data.map((item) => <Testlist key={item.id} data={item}/>) }</ul>
                     </ul> 
                     <TestConstructor nametest={this.state.nametest} />
                     <TestUpdate/>  
@@ -92,7 +100,7 @@ class Testlist extends React.Component{
             statue = "Majeur";
             cssAge = "ageMajeur"
         }
-        else if (age == 18){
+        else if (age === "18"){
             statue = "Viens d'être majeur";
             cssAge = "ageVMajeur"
         }
@@ -115,7 +123,7 @@ class TestConstructor extends React.Component{
     render(){
         return(
             <div>
-                <input type="text" value={this.props.nametest}/>
+                <input type="text" defaultValue={this.props.nametest}/>
                 <button onClick={this.handleEvent}> Cliquer ici</button>
             </div>
         );        
@@ -166,16 +174,22 @@ class TestLifeCycle extends React.Component{
     changeState(){
         this.setState({hello:"All great tuto"});
     }
-    componentWillMount(){
+    /** Adding Mounting Phase **/
+    UNSAFE_componentWillMount(){
         console.log('Component will mount')
+        setTimeout(()=> {this.setState({hello: "test Component will mount"})},1000)
+
     }
     componentDidMount(){
         console.log('Component did mount')
+        setTimeout(()=> {this.setState({hello: "test Component did mount"})},2000)
     }
     UNSAFE_componentWillReceiveProps(){
         console.log('Component will receive props')
     }
+    /** Updating phase **/
     shouldComponentUpdate(newProps, newState){
+        console.log('should component update'); 
         return true;
     }
     UNSAFE_componentWillUpdate(nextProps, nextState){
@@ -183,7 +197,8 @@ class TestLifeCycle extends React.Component{
     }
     componentDidUpdate(prevProps, prevState){
         console.log('Component did update')
-    }
+    }    
+    /** Deleting unmounting phase **/
     componentWillUnmount(){
         console.log('Component will Unmount')
     }
